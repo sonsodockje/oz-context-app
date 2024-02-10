d1f6db8d1e35c703be32f5a5a9f845a3921b2863
sonsodockje <hanaroo0115@naver.com>
2024-02-11 01:02:57 +0900

    ✨ 주문 확인 페이지

--- client/src/pages/SummaryPage/index.jsx
+++ client/src/pages/SummaryPage/index.jsx
@@ -1,10 +1,45 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { OrderContext } from "../../context/OrderContext";
 
 const SummaryPage = ({ setStep }) => {
+  const [OrderContextData] = useContext(OrderContext);
   const [checked, setChecked] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setStep(2);
+  };
+
+  // 맵을 배열로
+  const productArray = Array.from(OrderContextData.products);
+  const productList = productArray.map(([key, value]) => (
+    <li key={key}>
+      {value} {key}
+    </li>
+  ));
+
+  const hasOptions = OrderContextData.options.size > 0;
+  let optionsDisplay = null;
+
+  if (hasOptions) {
+    const optionsArray = Array.from(OrderContextData.options.keys());
+    const optionList = optionsArray.map((key) => <li key={key}>{key}</li>);
+
+    optionsDisplay = (
+      <>
+        <h2>옵션 가격 : {OrderContextData.totals.options}</h2>
+        <ul>{optionList}</ul>
+      </>
+    );
+  }
+
   return (
     <div>
-      <form>
+      <h2>주문 확인</h2>
+      <h2>여행 가격 : {OrderContextData.totals.products}</h2>
+      <ul>{productList}</ul>
+      {optionsDisplay}
+      <form onSubmit={handleSubmit}>
         <input
           type="checkbox"
           id="confirm_checkbox"
@@ -15,7 +50,7 @@ const SummaryPage = ({ setStep }) => {
         />{" "}
         <label htmlFor="confirm_checkbox">주문 확인을 해주세요.</label>
         <br />
-        <button disabled={!checked} onClick={() => setStep(2)}>
+        <button type="submin" disabled={!checked}>
           주문확인
         </button>
         <button onClick={() => setStep(0)}>취소</button>
